b726f795000a99a28dd35f187d8f50c2b6d2dae5
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-07-26 22:12:11 +0900

    事前準備

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  console.log("App");
+  const [count, setCount] = useState(0);
+  const onClickUp = () => {
+    setCount(count + 1);
+  };
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <p>{count}</p>
+      <button onClick={onClickUp}>カウントアップ</button>
     </div>
   );
 }
